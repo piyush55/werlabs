57fa595ee527e75225731d527a7062139ed55c69
Piyush Shrivastava <piyushsh898@gmail.com>
2017-07-12 14:20:41 +0530

    Integrate infinite scrolling

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,31 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PhotosService } from '../services/photos.service';
+import { MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent {
 
-  photos: any = [];
-  query: string = '';
+  private photos: any = [];
+  private query: string = '';
+  private errorMessage: string = '';
   
-  constructor(private photosService: PhotosService) { }
-
-  ngOnInit() {
-  }
+  constructor(private photosService: PhotosService, public snackBar: MdSnackBar) { }
 
   searchPhotos() {
+    this.errorMessage = '';
     if(this.query) {
+      let snackBarRef = this.snackBar.open('Searching...');
       this.photosService.getPhotos(this.query).subscribe(photos => {
-        this.photos = photos;
-        for (let photo of this.photos){
+        this.photos = this.photos.concat(photos);
+        for (let photo of this.photos) {
           photo['image'] = "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_m.jpg"
-
         }
+        snackBarRef.dismiss();
       });
+    }
+    else {
+      this.errorMessage = 'Input is required';
     }
   }
 
- 
+  onScroll() {
+    this.searchPhotos();
+	}
+}
