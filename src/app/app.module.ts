7f53ddb0499979743164e52e1c3d8a880b2e64bb
Piyush Shrivastava <piyushsh898@gmail.com>
2017-07-12 14:21:30 +0530

    Setup angular material to design UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
-
+import { MaterialModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll'
 import { AppComponent } from './app.component';
 import { PhotosComponent } from './photos/photos.component';
-import { PhotosService } from './photos/photos.service';
+import { PhotosService } from './services/photos.service';
+import { PhotoDetailsComponent } from './photo-details/photo-details.component';
 
 // Define the routes
 const ROUTES = [
@@ -18,18 +21,27 @@ const ROUTES = [
   {
     path: 'photos',
     component: PhotosComponent
-  }
+  },
+  {
+    path: 'photos/:id',
+    component: PhotoDetailsComponent,
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotosComponent
+    PhotosComponent,
+    PhotoDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
+    NoopAnimationsModule,
+    InfiniteScrollModule,
     RouterModule.forRoot(ROUTES) 
   ],
   providers: [PhotosService],
