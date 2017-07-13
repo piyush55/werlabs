b8d6404d8bcdf9c5ee9a8d73c08354dd7e896a8c
Piyush Shrivastava <piyushsh898@gmail.com>
2017-07-13 19:52:36 +0530

    Add test cases for list

--- src/app/photos/photos.component.spec.ts
+++ src/app/photos/photos.component.spec.ts
@@ -14,18 +14,17 @@ import { PhotosService } from '../services/photos.service'
 import { PhotosComponent } from './photos.component';
 
 class MockPhotosService {
-  public photos: any = { "photos":{ "page":1, "pages":2029, "perpage":100, "total":"202823",
+  public photos: any = { "page":1, "pages":2029, "perpage":100, "total":"202823",
             "photo":
               [
                 { "id":"35504939420", "owner":"25198384@N03", "secret":"2c15b5f698", "server":"4304", "farm":5, "title":"Dior", "ispublic":1, "isfriend":0, "isfamily":0 },
                 { "id":"35852011396", "owner":"149762420@N02", "secret":"efb241ffdf", "server":"4257", "farm":5, "title":"Detailn\u00ed",  "ispublic":1, "isfriend":0, "isfamily":0 },
                 { "id":"35892674745", "owner":"145094655@N06", "secret":"f43ff7ac8a", "server":"4214", "farm":5, "title":"#dogalize Razze cani: il cane Hokkaido, carattere e caratteristiche https:\/\/t.co\/5lUmy8U2TL #dogs #cats #pets https:\/\/t.co\/WL84K6Kjyj, dogalize", "ispublic":1, "isfriend":0, "isfamily":0 }
               ]
-            }
-          };
+            };
 
-  getPhotos() {
-    return Observable.of(this.photos);
+  getPhotos(text, page) {
+    return Observable.of(this.photos.photo);
   }
 }
 
@@ -62,5 +61,22 @@ describe('PhotosComponent', () => {
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('label').innerText).toEqual('Search photos by keyword');
       });
+  })));
+
+  it('Should list photos', async(inject([], () => {
+    component.query = 'nature';
+    fixture.detectChanges();
+    let searchBtn = fixture.debugElement.query(By.css('.mat-raised-button'));
+    searchBtn.triggerEventHandler('click');
+    fixture.whenStable()
+      .then(() => {
+        fixture.detectChanges();
+        return fixture.whenStable();
+      })
+      .then(() => {
+        const compiled = fixture.debugElement.nativeElement;
+        console.log("compiled==", compiled);
+        expect(compiled.querySelector('.mat-grid-tile a').getAttribute("href")).toEqual('/photos/35504939420');
+    });
   })));
 });
